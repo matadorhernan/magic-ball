e2afefdb1ac03f2819dd1ac28d203f19539e721f
Miguel Romero <miguelromereado@outlook.com>
2021-09-25 19:09:57 -0500

    add wip fi smartphone size

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component } from '@angular/core';
+import { replace } from 'feather-icons';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
-
+export class HomePage implements AfterViewInit {
   constructor() {}
 
+  ngAfterViewInit(): void {
+    replace();
+  }
 }
